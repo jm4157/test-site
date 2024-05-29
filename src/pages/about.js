80c825f2944846b30536552fc7a5d09e62150eaa
jm4157 <jm4157@rit.edu>
2024-05-29 13:37:49 -0400

    Completed part 2 of the Gatsby turtorial

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout title="Hellgorger's Howlers">
+      <p1>
+        Many people have been asking me "Ankhes, what was it like
+        playing for Hellgorger's Howlers?" Well dear fans, that's what
+        this page is meant to explain.
+      </p1>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Hellgorger's Howlers</title>
+
+export default AboutPage
